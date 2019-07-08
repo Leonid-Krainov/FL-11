a01c7e8b78d324d1f95800e1dd4ca3cd34dc2d49
Leonid-Krainov <KrajnovLeonid@gmail.com>
2019-07-08 23:32:13 +0300

    done homework7

--- FL11_HW7/homework/src/js/task1.js
+++ FL11_HW7/homework/src/js/task1.js
@@ -1,1 +1,45 @@
-// Your code goes here
+const minimalEmailLength = 6;
+const minimalPassLength = 5;
+let userEmail = prompt('Please enter email', '');
+if (userEmail === '' || userEmail === null) {
+  alert('Canceled.');
+} else if (userEmail.length < minimalEmailLength) {
+  alert('I don’t know any emails having name length less than 6 symbols');
+} else if (userEmail === 'user@gmail.com' || userEmail === 'admin@gmail.com') {
+  let password = prompt('Please enter password', '');
+  if (password === '' || password === null) {
+    alert('Canceled.');
+  } else if (userEmail === 'user@gmail.com' && password === 'UserPass' ||
+    userEmail === 'admin@gmail.com' && password === 'AdminPass') {
+    let changingPass = confirm('Do you want to change your password?');
+    if (changingPass === true) {
+      let checkPass = prompt('Please enter password', '');
+      if (checkPass === '' || checkPass === null) {
+        alert('Canceled.');
+      } else if (userEmail === 'user@gmail.com' && checkPass === 'UserPass' ||
+        userEmail === 'admin@gmail.com' && checkPass === 'AdminPass') {
+        let newPass = prompt('Please enter a new password', '');
+        if (newPass === '' || newPass === null) {
+          alert('Canceled.');
+        } else if (newPass.length < minimalPassLength) {
+          alert('It’s too short password. Sorry.');
+        } else {
+          let confirmNewPass = prompt('Please enter a new password again', '');
+          if (confirmNewPass === newPass) {
+            alert('You have successfully changed your password.');
+          } else {
+            alert('You wrote the wrong password.');
+          }
+        }
+      } else {
+        alert('Wrong password');
+      }
+    } else {
+      alert('You have failed the change.');
+    }
+  } else {
+    alert('Wrong password');
+  }
+} else {
+  alert('I don’t know you');
+}
--- FL11_HW7/homework/src/js/task2.js
+++ FL11_HW7/homework/src/js/task2.js
@@ -1,1 +1,36 @@
-// Your code goes here
+let stage = 'a game';
+while (confirm('Do you want to play ' + stage + '?') === true) {
+  let min = 0;
+  let max = 8;
+  let win = 0;
+  let bank = 100;
+  let startPrize = 100;
+  let attempts = 3;
+  let number = Math.floor(Math.random() * (max - min + 1)) + min;
+  console.log(number);
+  while (attempts > 0) {
+    let userNumber = prompt('Choose a roulette pocket number from ' + min + ' to ' + max + '\n' +
+      'Attempts left: ' + attempts + '\nTotal prize: ' + win + '$\n' +
+      'Possible prize on current attempt: ' + bank + '$');
+    if (parseInt(userNumber) !== number) {
+      bank = Math.floor(bank / 2);
+      attempts--;
+    } else {
+      win = win + bank;
+      let nextRound = confirm('Congratulation, you won! Your prize is ' + win + '$. Do you want to continue?');
+      if (nextRound === true) {
+        startPrize = startPrize * 2;
+        max = max + 4;
+        bank = startPrize;
+        attempts = 3;
+        number = Math.floor(Math.random() * (max - min + 1)) + min;
+        console.log(number);
+      } else {
+        break;
+      }
+    }
+  }
+  alert('Thank you for a game. Your prize is: ' + win + '$');
+  stage = 'again';
+}
+alert('You did not become a billionaire, but can');
